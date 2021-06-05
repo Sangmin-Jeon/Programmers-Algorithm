097db9c10b69b02004b54fc84e20c630ec876898
Sangmin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2021-06-05 21:34:42 +0900

    Add files via upload

---
+++ 문자열 내 마음대로 정렬하기/stringSort/stringSort/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  stringSort
+//
+//  Created by 전상민 on 2021/06/05.
+//
+
+import Foundation
+
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    let index = strings[0].index(strings[0].startIndex, offsetBy: n)
+ 
+    let result = strings.sorted(by : {(s1 : String, s2 : String) -> Bool in
+        if s1[index] == s2[index] {
+            return s1 < s2
+        }
+        else {
+            return s1[index] < s2[index]
+        }
+    })
+ 
+    return result
+}
+ 
+
+print(solution(["sun", "bed", "car"],1))
+
+
