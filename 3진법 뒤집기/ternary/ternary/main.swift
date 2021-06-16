0ca4fbab660d3a55c7fe3488b3454eb755cd3a53
Sangmin-Jeon <59474775+Sangmin-Jeon@users.noreply.github.com>
2021-06-16 14:49:31 +0900

    Update main.swift
    주석 수정

--- 3진법 뒤집기/ternary/ternary/main.swift
+++ 3진법 뒤집기/ternary/ternary/main.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+var arr:[Int] = []
+var result = 0
+var count = 1
+var num = 125
+
+while(num > 0){ // 3진법으로 변환
+   arr.append( num % 3)
+   num = num / 3
+}
+// stride() arr.count -1 부터 -1까지(-1은 포함 안됨), -1씩
+for i in stride(from: arr.count-1, to: -1, by: -1){
+   result = result + arr[i] * count // count의 초기값은 1
+   count =  count * 3 // 3제곱근
+
+}
+print(result)
+
+// --------------------------------------------------------------------------------
+// 잘 푼 예제 
+
+// let n  = 45
+
+// let flipToThree = String(n,radix: 3) // 3진법으로 변환, String Type이 된다.
+// let answer = Int(String(flipToThree.reversed()),radix:3)!
+// print(answer)
+
+// --------------------------------------------------------------------------------
+
 // 진수 변환 >
 // radix에 변환하고 싶은 진수의 수를 넣음
 // let decimal: Int = 30 // 10 진수는 Int Type
@@ -18,35 +46,6 @@ import Foundation
 // let decimal: Int = Int(binary, radix: 2)! // 문자가 숫자가 아닌경우 때문에 force Unrapping 해줌
 // print(decimal)
 /* 27 */
-
-// --------------------------------------------------------------------------------
-
-
-let n  = 45
-
-let flipToThree = String(n,radix: 3) // 3진법으로 변환, String Type이 된다.
-let answer = Int(String(flipToThree.reversed()),radix:3)!
-print(answer)
-
-// --------------------------------------------------------------------------------
-//var arr:[Int] = []
-//var result = 0
-//var count = 1
-//var num = 125
-//
-//while(num > 0){ // 3진법으로 변환
-//    arr.append( num % 3)
-//    num = num / 3
-//}
-//// stride() arr.count -1 부터 -1까지(-1은 포함 안됨), -1씩
-//for i in stride(from: arr.count-1, to: -1, by: -1){
-//    result = result + arr[i] * count // count의 초기값은 1
-//    count =  count * 3 // 3제곱근
-//
-//}
-//print(result)
-
-// --------------------------------------------------------------------------------
 
 // 제곱근 함수
 //func powerUsingFor(x: Int , a:Int) -> Int {
